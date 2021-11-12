5264ca740ba1402ec95944ec368e90e05125bbe3
Ian Ross <ianrossdrums@gmail.com>
2021-11-12 11:46:15 -0700

    Add model_a

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+# laptop hash with 8 keys and values
+LAPTOP = {
+  user_name: "Ian Ross",
+  password: "Turing!",
+  number_of_apps: 6,
+  screen_time_hours: 5,
+  is_on: true,
+  is_dirty: false,
+  updates: [1.1, 1.2, 1.3, 1.4, 1.5],
+  folders: ["Mod0", "Mod1 Pre-Work", "Mod1"]
+}
