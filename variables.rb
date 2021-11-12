daa8cf6b9ffb56c3158cf4ef5fd31a53cbfc1715
Ian Ross <ianrossdrums@gmail.com>
2021-11-12 11:36:17 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+animal = "Ferret"
+is_cuddly = true
+animal = "Evolved ferret"
+num_8 = 8
+num_3 = 2
+quotient = num_8 / num_3
+favorite_films = ["Star Wars", "Sword Art Online", "Labrynth"]
+favorite_films.shift
+p animal.length
