c339407f365d116eece0041b4dedbfc7013e98a6
Ian Ross <ianrossdrums@gmail.com>
2021-11-12 11:52:34 -0700

    Add model_b

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+HOUSE = {
+  year_house_bought: "2016",
+  fire_place_type: "electrical",
+  chairs: 6,
+  pets: 9,
+  is_clean: false,
+  light_on: true,
+  house_member: ["Ian", "Kirsten", "Lyndsey", "Mackenzie"],
+  pet_names: ["Pabu", "Loki", "Thor", "Minnie"]
+}
